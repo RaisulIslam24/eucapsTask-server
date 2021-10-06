a026a4f9151e87767edbee5c038ef5b534586194
Raisul Islam Nibir <raisulislam.cse3.bu@gmail.com>
2021-10-06 20:25:01 +0600

    Successfully Done

--- index.tsx
+++ index.tsx
@@ -11,7 +11,7 @@ const app = express()
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
   res.send("Welcome to Eucaps-task server.")
